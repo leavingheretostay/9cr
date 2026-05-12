914f6ba53085d78900d287603ecdfede48c22a3f
Nasir Lone <lonenasir724@gmail.com>
2026-05-12 23:33:31 +0530

    Update RichPresence.svelte

--- src/components/molecules/RichPresence.svelte
+++ src/components/molecules/RichPresence.svelte
@@ -1,74 +1,78 @@
 <h2>activity</h2>
 
 <div class="contain">
-	<img src="/nasir.jpg" alt="Nasir Lone" class="big" />
+	<img src="/default.webp" alt="Nasir" class="big" />
 
-	<div class="info">
+	<div class="text">
 		<h3>@nacirlone</h3>
-		<h5>Physics Postgraduate</h5>
-		<h5>drawn toward atoms, silence & the unknown</h5>
+		<h5>Offline</h5>
+		<h5>drawn toward atoms, silence & poetry</h5>
 	</div>
 </div>
 
 <style lang="scss">
-	.contain {
-		display: flex;
-		align-items: center;
-		gap: 1.4rem;
-		margin-top: 1rem;
-		margin-bottom: 4rem;
+	h2 {
+		display: none;
 	}
 
-	.info {
+	.contain {
+		display: flex;
+		gap: 2.25rem;
+		align-items: center;
+	}
+
+	.text {
 		display: flex;
 		flex-direction: column;
 	}
 
-	h2 {
-		display: block;
-		font-size: 3rem;
-		margin-bottom: 1rem;
-	}
-
 	h3 {
-		font-size: 2.2rem;
+		font-size: 3rem;
+		font-weight: 400;
+		line-height: 1;
 		margin: 0;
-		line-height: 1.1;
+		color: var(--text-primary);
 	}
 
 	h5 {
-		font-size: 1.2rem;
+		font-size: 1.5rem;
 		font-weight: 400;
-		margin: 0.2rem 0;
-		line-height: 1.3;
+		line-height: 1.4;
+		margin: 0;
 		color: var(--text-secondary);
 	}
 
 	.big {
-		height: 110px;
-		width: 110px;
-		border-radius: 18px;
+		height: 135px;
+		width: 135px;
+		border-radius: 20px;
 		object-fit: cover;
 		user-select: none;
 	}
 
 	@media screen and (max-width: 868px) {
+		h2 {
+			display: block;
+			margin-bottom: 1rem;
+		}
+
 		.contain {
-			gap: 1rem;
+			justify-content: left;
+			gap: 1.5rem;
 		}
 
 		.big {
-			height: 95px;
-			width: 95px;
-			border-radius: 16px;
+			height: 100px;
+			width: 100px;
+			border-radius: 17px;
 		}
 
 		h3 {
-			font-size: 1.8rem;
+			font-size: 2.2rem;
 		}
 
 		h5 {
-			font-size: 1rem;
+			font-size: 1.2rem;
 		}
 	}
 </style>
