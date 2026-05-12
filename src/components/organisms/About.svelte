d0786fe2ec5d0615e186f65b79290d475a62dc7e
Nasir Lone <lonenasir724@gmail.com>
2026-05-13 00:42:30 +0530

    Update About.svelte

--- src/components/organisms/About.svelte
+++ src/components/organisms/About.svelte
@@ -23,14 +23,14 @@
 	<div class="text">
 		<h2>bio</h2>
 		<p>
-	I'm <Tooltip tip="aka Bitta online"><span>Nasir Lone</span></Tooltip>, a <Tooltip tip={age}
+	I'm <Tooltip tip="aka bitta"><span>Nasir Lone</span></Tooltip>, a <Tooltip tip={age}
 		><span>{Math.floor(Number(age))}</span></Tooltip
-	> year old Physics postgraduate drawn toward <Tooltip tip="tiny universes"
+	> year old,a Physics postgraduate drawn toward <Tooltip tip="tiny universes"
 		><span>atoms</span></Tooltip
 	>, silence, coding, and design. I like exploring ideas, creating things, and finding beauty in both <Tooltip
 		tip="order in chaos"><span>science</span></Tooltip
 	> and <Tooltip tip="quiet expression"><span>art</span></Tooltip
-	>. Still learning, still wandering, and still trying to understand the universe one thought at a time.
+	>. Still learning. Still wandering A little tired but still curious.
 		</p> 
 	</div>
 </section>
