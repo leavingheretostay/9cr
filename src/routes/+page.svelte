fa11bf6a28b50382c279a9a3ba8ae727d2f44682
Nasir Lone <lonenasir724@gmail.com>
2026-05-12 22:35:09 +0530

    Update +page.svelte

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -12,7 +12,46 @@
 <main>
 	<Hero />
 	<About />
-	<Projects />
+	<section class="poems">
+	<h2>fragments</h2>
+
+	<div class="quote">
+		<p>
+			“Love each other or perish.”
+		</p>
+	</div>
+
+	<div class="quote">
+		<p>
+			“My atoms love your atoms,  
+			it’s chemistry.”
+		</p>
+	</div>
+
+	<div class="quote">
+		<p>
+			“Youth came over me like a mad storm.  
+			I was helpless to the chemicals roaring in my brain.”
+		</p>
+	</div>
+
+	<div class="quote">
+		<p>
+			“Poets spend their whole lives trying to think  
+			of a better way to say,  
+			‘He loved a girl.’”
+		</p>
+	</div>
+
+	<div class="quote">
+		<p>
+			“Sometimes I am terrified  
+			of my intense hunger to live,  
+			because dying has always seemed  
+			like the easier option.”
+		</p>
+	</div>
+</section>
 	<Art />
 	<Repos />
 	<Footer />
