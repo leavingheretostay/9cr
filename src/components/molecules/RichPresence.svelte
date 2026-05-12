6c2004749dfbd4496fcd442b356489100bba72d9
Nasir Lone <lonenasir724@gmail.com>
2026-05-13 00:03:23 +0530

    Update RichPresence.svelte

--- src/components/molecules/RichPresence.svelte
+++ src/components/molecules/RichPresence.svelte
@@ -1,84 +1,114 @@
 <script lang="ts">
-	let activity = "@nacirlone";
-	let details = "Offline";
-	let activityImage = "default.webp";
-	let state: string;
+    import { onMount } from 'svelte';
+    import Tooltip from '../atoms/Tooltip.svelte';
 
-	function localTime() {
-		state = new Date().toLocaleTimeString("en-IN", {
-			timeZone: "Asia/Kolkata"
-		});
-	}
+    let activity = '@nacirlone';
+    let details = 'Online';
+    let activityImage = 'your-profile-picture.jpg'; // Replace with your image path
+    let state: string;
+    let smallImage: string = '';
+    let isSpotify: boolean = false;
+    let isActivity: boolean = false;
+    let currentSetInterval: ReturnType<typeof setInterval>;
 
-	localTime();
-	setInterval(() => localTime(), 1000);
+    function localTime() {
+        state = new Date().toLocaleTimeString('en-US', { 
+            timeZone: 'Asia/Kolkata',
+            hour: '2-digit',
+            minute: '2-digit',
+            second: '2-digit',
+            hour12: true 
+        });
+    }
+
+    localTime();
+    currentSetInterval = setInterval(() => localTime(), 1000);
+
+    onMount(() => {
+        return () => {
+            clearInterval(currentSetInterval);
+        };
+    });
 </script>
 
 <h2>activity</h2>
-
 <div class="contain">
-	<img src={activityImage} alt={activity} class="big" />
-
-	<div>
-		<h3>{activity}</h3>
-		<h5>{details}</h5>
-		<h5>{state}</h5>
-	</div>
+    <img src={activityImage} alt={activity} class="big" class:spin={isSpotify} />
+    {#if smallImage}
+        <img src={smallImage} alt={activity} class="small" />
+    {/if}
+    <div>
+        <h3>{activity}</h3>
+        <h5>{details}</h5>
+        <h5>{state || ''}</h5>
+    </div>
 </div>
 
 <style lang="scss">
-	.contain {
-		display: flex;
-		gap: 2.25rem;
-		align-items: center;
-	}
+    .contain {
+        display: flex;
+        gap: 2.25rem;
+        align-items: center;
+    }
 
-	h2 {
-		display: none;
-	}
+    h2 {
+        display: none;
+    }
 
-	h3 {
-	font-size: 3.2rem;
-	font-weight: 400;
-	line-height: 1;
-	margin: 0;
-}
+    .big {
+        height: 135px;
+        width: 135px;
+        border-radius: 20px;
+        display: relative;
+        user-select: none;
+        transition: all 0.3s var(--bezier-one);
+    }
 
-h5 {
-	font-size: 1.35rem;
-	font-weight: 400;
-	line-height: 1.2;
-	margin: 0;
-	margin-top: 0.15rem;
-}
+    .small {
+        height: 40px;
+        width: 40px;
+        border-radius: 50%;
+        position: absolute;
+        transform: translate(275%, 150%);
+        outline: 6px solid var(--bg-color);
+        background-color: var(--bg-color);
+    }
 
-	.big {
-		height: 135px;
-		width: 135px;
-		border-radius: 20px;
-		user-select: none;
-		transition: all 0.3s var(--bezier-one);
-		object-fit: cover;
-	}
+    @keyframes rotate {
+        0% {
+            transform: rotate(0deg);
+        }
+        100% {
+            transform: rotate(360deg);
+        }
+    }
 
-	@media screen and (max-width: 868px) {
-		h2 {
-			display: block;
-			margin-bottom: 1rem;
-		}
+    .spin {
+        animation: rotate 40s linear infinite;
+        border-radius: 50%;
+    }
 
-		div {
-			justify-content: left;
-		}
+    @media screen and (max-width: 868px) {
+        h2 {
+            display: block;
+            margin-bottom: 1rem;
+        }
+        div {
+            justify-content: left;
+        }
 
-		.big {
-			height: 100px;
-			width: 100px;
-			border-radius: 17px;
-		}
+        .big {
+            height: 100px;
+            width: 100px;
+            border-radius: 17px;
+        }
 
-		h5 {
-			font-size: 1.4rem;
-		}
-	}
+        .spin {
+            border-radius: 50%;
+        }
+
+        .small {
+            transform: translate(190%, 110%);
+        }
+    }
 </style>
