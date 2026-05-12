31a51131d6b1fc6a6b34461daedb3a03c7cdea96
Nasir Lone <lonenasir724@gmail.com>
2026-05-12 23:15:53 +0530

    Update RichPresence.svelte

--- src/components/molecules/RichPresence.svelte
+++ src/components/molecules/RichPresence.svelte
@@ -1,58 +1,118 @@
-<script lang="ts">
-	let activity = '@nacirlone';
-	let details = 'Physics Postgraduate';
-	let state = 'drawn toward atoms, silence & the unknown';
-
-	let activityImage =
-		'https://raw.githubusercontent.com/leavingheretostay/9cr/main/static/nasir.jpg';
-</script>
-
-<h2>activity</h2>
-
-<div class="contain">
-	<img src={activityImage} alt="Nasir Lone" class="big" />
-
-	<div>
-		<h3>{activity}</h3>
-		<h5>{details}</h5>
-		<h5>{state}</h5>
-	</div>
-</div>
-
 <style lang="scss">
 	.contain {
 		display: flex;
-		gap: 2.25rem;
 		align-items: center;
+		gap: 1.4rem;
+		margin-top: 1rem;
 	}
 
 	h2 {
-		display: none;
+		font-size: 3rem;
+		margin-bottom: 1rem;
+	}
+
+	h3 {
+		font-size: 2.2rem;
+		margin: 0;
+		line-height: 1.1;
+	}
+
+	h5 {
+		font-size: 1.2rem;
+		font-weight: 400;
+		margin: 0.15rem 0;
+		line-height: 1.3;
+		color: var(--text-secondary);
+	}
+
+	a,
+	a:not(:hover) {
+		border-radius: 4px;
+		text-decoration: underline;
+		text-decoration-color: var(--bg-color);
+		transition: 0.3s var(--bezier-one);
+	}
+
+	a:hover {
+		text-decoration-color: var(--text-primary);
 	}
 
 	.big {
-		height: 135px;
-		width: 135px;
-		border-radius: 20px;
-		object-fit: cover;
+		height: 110px;
+		width: 110px;
+		border-radius: 18px;
 		user-select: none;
-		filter: grayscale(100%);
+		object-fit: cover;
+	}
+
+	.small {
+		height: 34px;
+		width: 34px;
+		border-radius: 50%;
+		position: absolute;
+		transform: translate(220%, 140%);
+		outline: 5px solid var(--bg-color);
+		background-color: var(--bg-color);
+	}
+
+	progress {
+		-webkit-appearance: none;
+		appearance: none;
+		border: 0;
+		border-radius: 10rem;
+		margin-top: 0.5rem;
+		background-color: var(--elevation-one);
+		height: 0.45rem;
+		overflow: hidden;
+		width: 100%;
+	}
+
+	progress::-webkit-progress-bar {
+		background-color: var(--elevation-one);
+		border-radius: 10rem;
+	}
+
+	progress::-webkit-progress-value {
+		background-color: var(--accent);
+		border-radius: 10rem;
+	}
+
+	progress::-moz-progress-bar {
+		background-color: var(--accent);
+		border-radius: 10rem;
+	}
+
+	@keyframes rotate {
+		0% {
+			transform: rotate(0deg);
+		}
+		100% {
+			transform: rotate(360deg);
+		}
+	}
+
+	.spin {
+		animation: rotate 40s linear infinite;
+		border-radius: 50%;
 	}
 
 	@media screen and (max-width: 868px) {
-		h2 {
-			display: block;
-			margin-bottom: 1rem;
-		}
-
-		div {
-			justify-content: left;
+		.contain {
+			gap: 1rem;
 		}
 
 		.big {
-			height: 100px;
-			width: 100px;
-			border-radius: 17px;
+			height: 95px;
+			width: 95px;
+			border-radius: 16px;
+		}
+
+		h3 {
+			font-size: 1.8rem;
+		}
+
+		h5 {
+			font-size: 1rem;
 		}
 	}
 </style>
