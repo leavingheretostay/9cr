a3a87506bbe0433332c91a85b175f9e4cb245b99
Nasir Lone <lonenasir724@gmail.com>
2026-05-12 21:45:25 +0530

    Update RichPresence.svelte

--- src/components/molecules/RichPresence.svelte
+++ src/components/molecules/RichPresence.svelte
@@ -1,8 +1,7 @@
 <script lang="ts">
 	let activity = '@nacirlone';
 	let details = 'Physics Postgraduate';
-	let state = 'drawn toward atoms, silence & the unknown';
-
+	
 	let activityImage =
 		'https://raw.githubusercontent.com/leavingheretostay/9cr/main/static/nasir.jpg';
 </script>
