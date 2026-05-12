cee647a19e0e6db3f42defe9b4be60309070fd5c
Nasir Lone <lonenasir724@gmail.com>
2026-05-12 21:10:29 +0530

    Update Socials.svelte

--- src/components/molecules/Socials.svelte
+++ src/components/molecules/Socials.svelte
@@ -1,32 +1,31 @@
 <script>
 	import Social from '../atoms/Social.svelte';
-	import { user } from '../../util/discord';
 
-	import { GitHubIcon } from '@indaco/svelte-iconoir/github';
-	import { DiscordIcon } from '@indaco/svelte-iconoir/discord';
+	import { InstagramIcon } from '@indaco/svelte-iconoir/instagram';
+	import { XIcon } from '@indaco/svelte-iconoir/x';
 	import { MailIcon } from '@indaco/svelte-iconoir/mail';
 	import { CoffeeCupIcon } from '@indaco/svelte-iconoir/coffee-cup';
 	import { LinkedInIcon } from '@indaco/svelte-iconoir/linkedin';
 </script>
 
 <div class="socials-container">
-	<Social tip="xafn" link="https://github.com/xafn">
-		<GitHubIcon color="var(--accent)" />
+	<Social tip="@nacirlone" link="https://instagram.com/nacirlone">
+		<InstagramIcon color="var(--accent)" />
 	</Social>
 
-	<Social tip="affn" link="https://linkedin.com/in/affn">
+	<Social tip="Nasir Lone" link="https://linkedin.com">
 		<LinkedInIcon color="var(--accent)" />
 	</Social>
 
-	<Social tip={user.username} link={`https://discord.com/users/${user.id}`}>
-		<DiscordIcon color="var(--accent)" />
+	<Social tip="@beingkashmire" link="https://twitter.com/beingkashmire">
+		<XIcon color="var(--accent)" />
 	</Social>
 
-	<Social tip="hi@affa.nz" link="mailto:hi@affa.nz">
+	<Social tip="mail me" link="mailto:lonenasir724@gmail.com">
 		<MailIcon color="var(--accent)" />
 	</Social>
 
-	<Social tip="affan" link="https://ko-fi.com/affan">
+	<Social tip="bitta" link="#">
 		<CoffeeCupIcon color="var(--accent)" />
 	</Social>
 </div>
